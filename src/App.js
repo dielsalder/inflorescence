17865addd4d4e9aeaf6ee4049aa8054605a6486c
dielsalder <ehe@mit.edu>
2020-01-13 23:50:29 -0500

    trying to implement react inputs but not sure about it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import Scene from './Scene';
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      numPetals: 12,
+      petalPitch:30*Math.PI/180,
+      petalLength : 6,
+      petalInner : -1,
+      petalOuter : 3,
+
+      centerSides : 6,
+      centerBottomRadius : 0,
+      centerTopRadius : 1,
+      centerHeight : .5,
+      centerTranslateZ : 0.5,
+    }
+  }
+  changeNpetals(event){
+    this.setState({
+      numPetals:event.target.value
+    });
+  }
   render() {
     return (
       <div>
-        <Scene />
+        <label> number of petals </label>
+        <input type="range" name = "nPetals" 
+          onChange={this.changeNpetals.bind(this)}
+          min = "0"
+          max = "24"
+          value = {this.state.numPetals}/>
+
+        <Scene {...this.state} />
       </div>
     );
   }
